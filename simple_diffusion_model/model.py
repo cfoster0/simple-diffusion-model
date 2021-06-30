6568d01534d1f39ebc2cd3b28b90b5d98c54bf10
Charles Foster <cfoster0@stanford.edu>
2021-06-29 23:21:44 -0700

    Model empty

--- simple_diffusion_model/model.py
+++ simple_diffusion_model/model.py
@@ -3,3 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from einops import rearrange, reduce, repeat
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+       
+    def forward(self, x):
+        return x
