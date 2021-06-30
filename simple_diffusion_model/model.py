4c14e2a53c4864db793c2f5ebfa303d667531c20
Charles Foster <cfoster0@stanford.edu>
2021-06-29 23:31:31 -0700

    Add Fourier features

--- simple_diffusion_model/model.py
+++ simple_diffusion_model/model.py
@@ -4,9 +4,20 @@ import torch.nn.functional as F
 
 from einops import rearrange, reduce, repeat
 
-class Model(nn.Module):
-    def __init__(self):
+class FourierFeatures(nn.Module):
+    def __init__(self, in_features, out_features, std=1.0):
         super().__init__()
+        self.mapping = torch.randn(mean=0, std=std, size=(out_features // 2, in_features))
        
     def forward(self, x):
+        projection = self.mapping @ x
+        embedding = torch.cat(torch.cos(projection), torch.sin(projection), -1)
+        return embedding
+        
+class Model(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.timestep_to_embedding = FourierFeatures(1, dim)
+       
+    def forward(self, x, timestep):
         return x
