8568e0d87817ab7c95f2ea7e43ea290309f520dd
Charles Foster <cfoster0@stanford.edu>
2021-06-30 19:53:54 -0700

    Fix and use rotary for timestep-conditioned features

--- simple_diffusion_model/model.py
+++ simple_diffusion_model/model.py
@@ -7,7 +7,7 @@ from einops import rearrange, reduce, repeat
 class RotaryFeatures(nn.Module):
     def __init__(self, out_features):
         super().__init__()
-        inv_freq = 1. / torch.logspace(1.0, 100.0, out_features // 2)
+        inv_freq = 1. / torch.logspace(1.0, 10_000.0, out_features // 2)
         self.register_buffer('inv_freq', inv_freq)
         
     def forward(self, x, timestep):
@@ -18,21 +18,12 @@ class RotaryFeatures(nn.Module):
         rotated = torch.cat((-evens, odds), dim = -1)
         out += rotated * posemb.sin()
         return out
-
-class FourierFeatures(nn.Module):
-    def __init__(self, out_features, std=1.0):
-        super().__init__()
-        self.mapping = torch.randn(mean=0, std=std, size=(out_features // 2, 1))
-       
-    def forward(self, timestep):
-        projection = self.mapping @ timestep
-        embedding = torch.cat(torch.cos(projection), torch.sin(projection), -1)
-        return embedding
         
 class Model(nn.Module):
     def __init__(self, dim):
         super().__init__()
-        self.timestep_to_embedding = RotaryFeaturesFeatures(dim)
+        self.timestep_to_embedding = RotaryFeatures(dim)
        
     def forward(self, x, timestep):
+        x = self.timestep_to_embedding(x, timestep)
         return x
