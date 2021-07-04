89f40172029fbccc936dbd90993c36759def22b9
Charles Foster <cfoster0@stanford.edu>
2021-07-04 11:52:38 -0700

    Imports on train

--- simple_diffusion_model/train.py
+++ simple_diffusion_model/train.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 from einops import rearrange, reduce, repeat
+from typing import Sequence, Tuple, Callable
+from torch.nn import Module, Linear, LayerNorm, GroupNorm
