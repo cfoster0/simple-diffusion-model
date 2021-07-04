4be3d126b6c1c92fd3e8e98b627bcaeaa4ca9127
Charles Foster <cfoster0@stanford.edu>
2021-07-04 16:16:59 -0700

    Work on diffusion wrapper

--- simple_diffusion_model/diffusion_wrapper.py
+++ simple_diffusion_model/diffusion_wrapper.py
@@ -5,14 +5,17 @@ import numpy as np
 
 from einops import rearrange, reduce, repeat
 from typing import Sequence, Tuple, Callable
-from torch.nn import Module, Linear, LayerNorm, GroupNorm
+from torch.nn import Module
 
+def beta_schedule(timesteps):
+    pass
 
 class DiffusionWrapper(nn.Module):
     def __init__(self, net, timesteps=1000):
         super().__init__()
         self.net = net
         self.timesteps = timesteps
+        self.beta_schedule = beta_schedule(timesteps)
 
     @torch.no_grad()
     def generate(self, **kwargs):
@@ -21,5 +24,14 @@ class DiffusionWrapper(nn.Module):
         self.net.train(was_training)
         return out
 
-    def forward(self, x, timestep, **kwargs):
-        return self.net(x, timestep)
+    def forward(self, x, **kwargs):
+        b = x.shape[0]
+
+        timestep = torch.randint(0, self.timesteps, (b))
+        noised = x
+        noise = noised - x
+        predicted_noise = self.net(noised, timestep)
+        loss = F.mse_loss(predicted_noise, noise)
+        return noise
+        
+        
