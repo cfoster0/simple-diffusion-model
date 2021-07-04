de0f31a216df2fffaee69b75a672569d4ac25498
Charles Foster <cfoster0@stanford.edu>
2021-07-04 16:23:36 -0700

    Generate function detail sketch

--- simple_diffusion_model/diffusion_wrapper.py
+++ simple_diffusion_model/diffusion_wrapper.py
@@ -11,18 +11,22 @@ def beta_schedule(timesteps):
     pass
 
 class DiffusionWrapper(nn.Module):
-    def __init__(self, net, timesteps=1000):
+    def __init__(self, net, input_shape, timesteps=1000):
         super().__init__()
         self.net = net
+        self.input_shape = input_shape
         self.timesteps = timesteps
         self.beta_schedule = beta_schedule(timesteps)
 
     @torch.no_grad()
-    def generate(self, **kwargs):
+    def generate(self, n, **kwargs):
         was_training = self.net.training
         self.net.eval()
+        x = torch.randn((n,) + input_shape)
+        for timestep in self.timesteps:
+            x = x - self.net(x, timestep)
         self.net.train(was_training)
-        return out
+        return x
 
     def forward(self, x, **kwargs):
         b = x.shape[0]
