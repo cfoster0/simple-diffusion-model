ad7d0432fdf64c4cec473611bbb60c215e4f7ecf
Charles Foster <cfoster0@stanford.edu>
2021-07-04 20:55:57 -0700

    Delete train file to move to examples

--- simple_diffusion_model/train.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-
-from einops import rearrange, reduce, repeat
-from typing import Sequence, Tuple, Callable
-from torch.nn import Module, Linear, LayerNorm, GroupNorm
