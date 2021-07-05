9261b0da0e43329f48138ee2104db12e4882c954
Charles Foster <cfoster0@stanford.edu>
2021-07-04 21:00:56 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'simple-diffusion-model',
+  packages = find_packages(exclude=['examples']),
+  version = '0.0.1',
+  license='GPL-3',
+  description = 'Simple Diffusion Model',
+  author = 'Charles Foster',
+  author_email = 'cfoster0@alumni.stanford.edu',
+  url = 'https://github.com/cfoster0/simple-diffusion-model',
+  keywords = [
+    'artificial intelligence',
+  ],
+  install_requires=[
+    'torch',
+    'einops',
+    'numpy',
+    'wandb',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
