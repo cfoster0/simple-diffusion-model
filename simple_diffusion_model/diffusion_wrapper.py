cc4a217725045726001492699ffb1b6ff35ea11b
Charles Foster <cfoster0@stanford.edu>
2021-07-05 00:20:07 -0700

    Convert from numpy tensor

--- simple_diffusion_model/diffusion_wrapper.py
+++ simple_diffusion_model/diffusion_wrapper.py
@@ -34,7 +34,7 @@ class DiffusionWrapper(nn.Module):
     def forward(self, x, **kwargs):
         noise = torch.randn(x.shape)
         timestep = torch.randint(0, self.timesteps, (x.shape[0],))
-        alpha_hat = torch.gather(self.alpha_hat_schedule, 0, timestep)
+        alpha_hat = torch.gather(torch.from_numpy(self.alpha_hat_schedule), 0, timestep)
         noised = alpha_hat.sqrt() * x + (1.0 - alpha_hat).sqrt() * noise
         predicted_noise = self.net(noised, timestep)
         loss = F.mse_loss(predicted_noise, noise)
