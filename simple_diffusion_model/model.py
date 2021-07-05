d3a370f835e8b4012eb45b9acb24c76e183eb84f
Charles Foster <cfoster0@stanford.edu>
2021-07-04 23:51:09 -0700

    Add modulelist

--- simple_diffusion_model/model.py
+++ simple_diffusion_model/model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from einops import rearrange, reduce, repeat
 from typing import Sequence, Tuple, Callable
-from torch.nn import Module, Linear, LayerNorm, GroupNorm
+from torch.nn import Module, ModuleList, Linear, LayerNorm, GroupNorm
 
 
 class Rotary(Module):
