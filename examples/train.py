b6a354f6890ed096e195219ccf20f6bf58e741a0
Charles Foster <cfoster0@stanford.edu>
2021-07-04 20:57:56 -0700

    Move train file to examples

---
+++ examples/train.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from einops import rearrange, reduce, repeat
+from typing import Sequence, Tuple, Callable
