b7467f47d0384c6ebef4ee40eafba9b8eda8a03e
Charles Foster <cfoster0@stanford.edu>
2021-07-05 21:41:30 -0700

    Make timestep a proper tensor

--- simple_diffusion_model/diffusion_wrapper.py
+++ simple_diffusion_model/diffusion_wrapper.py
@@ -24,7 +24,8 @@ class DiffusionWrapper(Module):
         self.net.eval()
         x = torch.randn((n,) + self.input_shape).cuda()
         for t in reversed(range(self.timesteps)):
-            x = (self.alpha_schedule[t] ** -0.5) * (x - ((1.0 - self.alpha_schedule[t]) * (1.0 - self.alpha_hat_schedule[t]) ** -0.5) * self.net(x, t))
+            timestep = torch.full((n,), t)
+            x = (self.alpha_schedule[t] ** -0.5) * (x - ((1.0 - self.alpha_schedule[t]) * (1.0 - self.alpha_hat_schedule[t]) ** -0.5) * self.net(x, timestep))
             if t > 0:
                 z = torch.randn((n,) + self.input_shape)
                 x += (self.beta_schedule[t] ** 0.5) * z
