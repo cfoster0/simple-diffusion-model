28769c01e1fabcd4354b66d3964c02dd96bd185b
Charles Foster <cfoster0@stanford.edu>
2021-07-06 15:09:40 -0700

    Hat -> bar

--- simple_diffusion_model/diffusion_wrapper.py
+++ simple_diffusion_model/diffusion_wrapper.py
@@ -16,7 +16,7 @@ class DiffusionWrapper(Module):
         self.timesteps = timesteps
         self.register_buffer('beta_schedule', torch.from_numpy(beta_schedule(timesteps)))
         self.register_buffer('alpha_schedule', torch.from_numpy(1.0 - beta_schedule(timesteps)))
-        self.register_buffer('alpha_hat_schedule', torch.from_numpy(np.cumprod(1.0 - beta_schedule(timesteps))))
+        self.register_buffer('alpha_bar_schedule', torch.from_numpy(np.cumprod(1.0 - beta_schedule(timesteps))))
 
     @torch.no_grad()
     def generate(self, n, *args, **kwargs):
@@ -26,7 +26,7 @@ class DiffusionWrapper(Module):
         x = torch.randn((n,) + self.input_shape, device=device)
         for t in reversed(range(self.timesteps)):
             timestep = torch.full((n,), t, device=device)
-            x = (self.alpha_schedule[t] ** -0.5) * (x - ((1.0 - self.alpha_schedule[t]) * (1.0 - self.alpha_hat_schedule[t]) ** -0.5) * self.net(x, timestep, *args, **kwargs))
+            x = (self.alpha_schedule[t] ** -0.5) * (x - ((1.0 - self.alpha_schedule[t]) * (1.0 - self.alpha_bar_schedule[t]) ** -0.5) * self.net(x, timestep, *args, **kwargs))
             if t > 0:
                 z = torch.randn((n,) + self.input_shape, device=device)
                 x += (self.beta_schedule[t] ** 0.5) * z
@@ -38,8 +38,8 @@ class DiffusionWrapper(Module):
         x = x.to(device)
         noise = torch.randn(x.shape, device=device)
         timestep = torch.randint(0, self.timesteps, (x.shape[0],), device=device)
-        alpha_hat = torch.gather(self.alpha_hat_schedule, 0, timestep)
-        noised = einsum("b , b ... -> b ...", alpha_hat ** 0.5, x) + einsum("b , b ... -> b ...", (1.0 - alpha_hat) ** 0.5, noise)
+        alpha_bar = torch.gather(self.alpha_bar_schedule, 0, timestep)
+        noised = einsum("b , b ... -> b ...", alpha_hat ** 0.5, x) + einsum("b , b ... -> b ...", (1.0 - alpha_bar) ** 0.5, noise)
         predicted_noise = self.net(noised, timestep, *args, **kwargs)
         loss = F.mse_loss(predicted_noise, noise)
         return loss
